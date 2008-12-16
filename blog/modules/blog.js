848036652169d126ffa105a82d6c9db2ae363e40
meh <meh.ffff@gmail.com>
2008-12-16 04:26:42 +0100

    Improvements.
    Improved the blog module.

--- blog/modules/blog.js
+++ blog/modules/blog.js
@@ -1,7 +1,5 @@
 miniLOL.module.create('blog', {
     onLoad: function() {
-        this._error = false;
-
         new Ajax.Request('resources/blog.xml', {
             method: 'get',
             asynchronous: false,
@@ -10,16 +8,24 @@ miniLOL.module.create('blog', {
                 http.responseXML.$ = _$;
                 miniLOL._cache.blog = http.responseXML;
             }
-
-            onFailure: function () {
-                miniLOL.module.list.blog._error = true;
-            }
         });
+
+        setTimeout("miniLOL.module.reload('blog')", miniLOL.config.refreshEvery*1000);
     },
 
     execute: function (args) {
-        if (this._error) {
+        if (!miniLOL._cache.blog) {
             miniLOL.config.contentNode.innerHTML = "An error occurred while loading blog.xml";
+        }
+        
+        if (args.id) {
+            var post = miniLOL._cache.blog.$(args.id);
+            if (post) {
+                miniLOL.config.contentNode.innerHTML = post.firstChild.nodeValue;
+            }
+            else {
+                miniLOL.config.contentNode.innerHTML = "Post not found.";
+            }
         }
     }
 });
