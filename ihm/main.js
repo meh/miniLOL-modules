046732a30155ab643039298b0a087eb1c8eb6ee2
meh <meh.ffff@gmail.com>
2010-01-09 19:51:35 +0100

    Improved the position of the message in IHM.

--- ihm/main.js
+++ ihm/main.js
@@ -24,17 +24,29 @@ miniLOL.module.create('ihm', {
             return;
         }
 
+        function resize () {
+            $('yiff').setStyle({
+                top:  (((document.viewport.getHeight() - $('yiff').getHeight())/2)-20)+'px',
+                left: ((document.viewport.getWidth() - $('yiff').getWidth() )/2)+'px'
+            });
+        }
+
+        Event.observe(window, 'resize', resize);
+
         var block = new Element("div", { style: "width: 100%; height: 100%; z-index: 9001; position: absolute; top: 0; left: 0; background: black; color: white; font-weight: bold; font-size: 23px; text-align: center; opacity: 0.9; filter: alpha(opacity = 90);"});
 
         document.body.appendChild(block);
-        block.update("<div style='position: relative; top: 48%;'>" +
+        block.update("<div id='yiff' style='position: absolute;'>" +
             "Are you aware that you're using a SHITTY browser?<br/><br/>" +
             "If you aren't, get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.<br/><br/>" +
             "If you are, <span style='color: red;'>yiff in hell furfag</span>."
         + "</div>");
 
+        resize();
+
         setTimeout(function () {
-            block.parentNode.removeChild(block);            
+            block.parentNode.removeChild(block);
+            Event.stopObserving(window, 'resize', resize);
         }, seconds * 1000);
     }
 });
