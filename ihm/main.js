1a5209a24884d5ef4cd28669caf9c4839769fd26
meh <meh.ffff@gmail.com>
2010-01-10 18:51:23 +0100

    Fixed some shit in IHM.

--- ihm/main.js
+++ ihm/main.js
@@ -10,10 +10,10 @@
  *  0. You just DO WHAT THE FUCK YOU WANT TO.                        *
  *********************************************************************/
 
-miniLOL.module.create('ihm', {
-    version: '0.1',
+miniLOL.module.create("ihm", {
+    version: "0.1",
 
-    type: 'passive',
+    type: "passive",
 
     initialize: function () {
         this.execute(23);
@@ -25,28 +25,35 @@ miniLOL.module.create('ihm', {
         }
 
         function resize () {
-            $('yiff').setStyle({
-                top:  (((document.viewport.getHeight() - $('yiff').getHeight())/2)-20)+'px',
-                left: ((document.viewport.getWidth() - $('yiff').getWidth() )/2)+'px'
+            $("yiff").setStyle({
+                top:  ((document.viewport.getHeight() - $("yiff").getHeight())/2) + "px",
+                left: ((document.viewport.getWidth() - $("yiff").getWidth() )/2) + "px"
             });
         }
 
-        Event.observe(window, 'resize', resize);
+        var block = new Element("div", {
+            style: "z-index: 9001; position: absolute; top: 0; left: 0; background: black; color: white; font-weight: bold; font-size: 23px; text-align: center;"
+        });
 
-        var block = new Element("div", { style: "width: 100%; height: 100%; z-index: 9001; position: absolute; top: 0; left: 0; background: black; color: white; font-weight: bold; font-size: 23px; text-align: center; opacity: 0.9; filter: alpha(opacity = 90);"});
+        block.setStyle({
+            width:  document.viewport.getWidth() + "px",
+            height: document.viewport.getHeight() + "px"
+        });
 
-        document.body.appendChild(block);
         block.update("<div id='yiff' style='position: absolute;'>" +
             "Are you aware that you're using a SHITTY browser?<br/><br/>" +
             "If you aren't, get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.<br/><br/>" +
             "If you are, <span style='color: red;'>yiff in hell furfag</span>."
         + "</div>");
 
-        resize();
+        $(document.body).insert(block);
+
+        Event.observe(window, "resize", resize);
+        resize.defer();
 
         setTimeout(function () {
-            block.parentNode.removeChild(block);
-            Event.stopObserving(window, 'resize', resize);
+            block.remove();
+            Event.stopObserving(window, "resize", resize);
         }, seconds * 1000);
     }
 });
