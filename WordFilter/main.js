93661ed30a4147af66c170be2fe4f8434ca19506
meh <meh.ffff@gmail.com>
2010-01-16 00:01:49 +0100

    Added the license block to WordFilter.

--- WordFilter/main.js
+++ WordFilter/main.js
@@ -1,6 +1,14 @@
-/*
- * This is just a faggy module, it does simple channysh wordfiltering.
- */
+/*********************************************************************
+*           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE              *
+*                   Version 2, December 2004                         *
+*                                                                    *
+*  Copyleft meh.                                                     *
+*                                                                    *
+*           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE              *
+*  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION   *
+*                                                                    *
+*  0. You just DO WHAT THE FUCK YOU WANT TO.                         *
+*********************************************************************/
 
 (function () {
 
