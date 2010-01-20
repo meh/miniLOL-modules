3714cf54bcecbc2e732dc06682d0bac1b72018e8
meh <meh.ffff@gmail.com>
2010-01-20 23:40:27 +0100

    meh.

--- IHM/main.js
+++ IHM/main.js
@@ -16,40 +16,72 @@ miniLOL.module.create("IHM", {
     type: "passive",
 
     initialize: function () {
-        this.execute(23);
+        if (Prototype.Browser.IE) {
+            this.wait = 23;
+
+            this.message
+                = "Are you aware that you're using a SHITTY browser?<br/><br/>"
+                + "If you aren't, get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.<br/><br/>"
+                + "If you are, <span style='color: red;'>yiff in hell furfag</span>.";
+        }
+        /*
+        else if (Prototype.Browser.Safari) {
+            this.wait = 15;
+
+            this.message
+                = "Why should I let a macfag in? At least why should I let you join instantly? :3<br/><br/>"
+                + "WebKit is theoretically good, but, sadly, it sucks in some parts.<br/><br/>"
+                + "For example it doesn't apply CSS added to a style tag via javascript.<br/><br/>"
+                + "<span style='color: red;'>APPLE IS FOR FAGGOTS</span><br/><br/>"
+                + "Get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.";
+        }
+        else if (Prototype.Browser.Chrome) {
+            this.wait = 10;
+
+            this.message
+                = "Google is EVIL, its motto is \"don't be evil\" because it wants to be the only evil entity >:(<br/><br/>"
+                + "Also V8 sucks hard, it's shit compared to TraceMonkey, it's faster because it has less features.<br/><br/>"
+                + "<span style='color: blue;'>G</span><span style='color: red;'>o</span><span style='color: yellow;'>o</span><span style='color: blue;'>g</span><span style='color: green;'>l</span><span style='color: red;'>e</span>geddon is near.<br/><br/>"
+                + "Get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.";
+        }
+        */
+
+        this.execute(this.wait);
     },
 
     execute: function (seconds) {
-        if (!Prototype.Browser.IE) {
+        if (!this.message) {
             return;
         }
 
-        function resize () {
+        function resize (lol) {
+            $("IHM").setStyle({
+                width:  document.viewport.getWidth() + "px",
+                height: document.viewport.getHeight() + "px"
+            });
+
             $("yiff").setStyle({
                 top:  ((document.viewport.getHeight() - $("yiff").getHeight())/2) + "px",
-                left: ((document.viewport.getWidth() - $("yiff").getWidth() )/2) + "px"
+                left: ((document.viewport.getWidth() - $("yiff").getWidth())/2) + "px"
             });
+
+            if (lol) {
+                resize.defer();
+            }
         }
 
         var block = new Element("div", {
+            id:    "IHM",
             style: "z-index: 9001; position: absolute; top: 0; left: 0; background: black; color: white; font-weight: bold; font-size: 23px; text-align: center;"
         });
 
-        block.setStyle({
-            width:  document.viewport.getWidth() + "px",
-            height: document.viewport.getHeight() + "px"
-        });
-
-        block.update("<div id='yiff' style='position: absolute;'>" +
-            "Are you aware that you're using a SHITTY browser?<br/><br/>" +
-            "If you aren't, get a real browser, get <a href='http://getfirefox.com' style='font-size: 23px;'>Firefox</a>.<br/><br/>" +
-            "If you are, <span style='color: red;'>yiff in hell furfag</span>."
-        + "</div>");
+        block.update("<div id='yiff' style='position: absolute;'>" + this.message + "</div>");
 
         $(document.body).insert(block);
 
         Event.observe(window, "resize", resize);
-        resize.defer();
+        Event.observe(document, ":initialized", resize);
+        resize.defer(true);
 
         setTimeout(function () {
             block.remove();
