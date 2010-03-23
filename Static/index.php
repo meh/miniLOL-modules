a64796253cdf6889d082f1290380f9f7fa9d7b6d
meh <meh.ffff@gmail.com>
2010-03-23 20:23:44 +0100

    Text only version of miniLOL.

---
+++ Static/index.php
@@ -0,0 +1,141 @@
+<?php
+/*********************************************************************
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE             *
+ *                   Version 2, December 2004                        *
+ *                                                                   *
+ *  Copyleft meh.                                                    *
+ *                                                                   *
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE             *
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION  *
+ *                                                                   *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.                        *
+ *********************************************************************/
+
+define('__VERSION__', '0.1');
+
+$config = @simplexml_load_file('modules/Static/resources/config.xml');
+
+if ($config && $config->root) {
+    $root = $config->root;
+}
+else {
+    $root = 'index.html';
+}
+
+$config = simplexml_load_file('resources/config.xml');
+
+$query = $_SERVER['QUERY_STRING'];
+
+if ($query) {
+    preg_match('/^([^=]+)(&|$)/', $query, $matches);
+
+    if ($matches) {
+        $page = $matches[1];
+    }
+    else {
+        preg_match('/^page=(.+?)(&|$)/', $query, $matches);
+
+        if ($matches) {
+            $page     = $matches[1];
+            $external = true;
+        }
+    }
+}
+
+if (!$page) {
+    $page = $config->homePage;
+}
+
+if ($external) {
+    $page = preg_replace('/\.\.\/g', '', $page);
+
+    $content = @file_get_contents($page);
+
+    if (!$content) {
+        $content = '404 - File not found';
+    }
+}
+else {
+    function parse ($node) {
+        $result = '';
+
+        foreach ($node->childNodes as $element) {
+            $text = $element->nodeValue;
+
+            if ($text = preg_replace('/^\s+$/m', '', $text)) {
+                $result .= preg_replace('/href=(["\'])#/', 'href=$1?', $text);
+            }
+        }
+
+        return $result;
+    }
+
+    $pages = DOMDocument::load('resources/pages.xml');
+    $pages->normalizeDocument();
+
+    $page = $pages->getElementById($page);
+
+    if (!$page) {
+        $content = '404 - File not found';
+    }
+    else {
+        $content = parse($page);
+    }
+}
+
+$menus = DOMDocument::load('resources/menus.xml');
+
+$default = $menus->getElementById('default');
+
+if (!$default) {
+    foreach ($default->childNodes as $node) {
+        if ($node->nodeType == XML_ELEMENT_NODE) {
+            $default = $node;
+            break;
+        }
+    }
+}
+
+$menu  = '';
+
+foreach ($default->childNodes as $node) {
+    if ($node->nodeName == 'item') {
+        $href = preg_replace('/#/', '?', $node->getAttribute('href'));
+        $text = $node->firstChild->nodeValue;
+        $menu .= "<a href='{$href}'>{$text}</a>&nbsp;";
+    }
+}
+
+echo <<<HTML
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{$config->siteTitle}</title>
+    
+    <script>
+        location.href = "{$root}";
+    </script>
+</head>
+
+<body>
+    <noscript>
+        <div style="position: absolute; top: 10px; left: 10px; border: 2px solid black; background: white;">If you have Javascript disabled, enable it please, or the website will be shown in the text only version.</div>
+
+        <br/><br/>
+
+        <div class="menu">
+            {$menu}
+        </div>
+
+        <br/>
+
+        <div class="body">
+            {$content}
+        </div>
+    </noscript>
+</body>
+</html>
+
+HTML
+?>
